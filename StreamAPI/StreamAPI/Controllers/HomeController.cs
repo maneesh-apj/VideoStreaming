be03178100dc4b4df6b6a1e2b952b1861adf5aea
maneesh-apj <maneesh.apj@hotmail.com>
2025-02-10 00:00:58 +0530

    API
    commit and check in

---
+++ StreamAPI/StreamAPI/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StreamAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("stream/{fileName}")]
+        public IActionResult StreamVideo(string fileName)
+        {
+            var filePath = Path.Combine("Videos", fileName);
+            if (!System.IO.File.Exists(filePath))
+            {
+                return NotFound("Video not found.");
+            }
+
+            var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            return File(stream, "video/mp4", enableRangeProcessing: true);
+        }
+    }
+}
